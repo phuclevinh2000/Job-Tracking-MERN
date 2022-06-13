4c95fde090640647e8e65af51f68ae5a6b5a6856
Phuc Le <phuclevinh2000@gmail.com>
2022-06-14 00:00:34 +0300

    refactor the error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,19 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.StatusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.StatusCode = StatusCodes.NOT_FOUND;
-  }
-}
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new NotFoundError('Please provide all values!');
+    throw new BadRequestError('Please provide all values!');
   }
 
+  const userAlreadyExists = await User.findOne({ email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
