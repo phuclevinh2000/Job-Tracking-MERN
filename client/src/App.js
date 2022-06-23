9dbeb08c3c5fbd1629398644ab5c92a10b0a3f9d
Phuc Le <phuclevinh2000@gmail.com>
2022-06-23 15:36:08 +0300

    Creating protecting route for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Landing, Register, Error } from './pages';
+import { Landing, Register, Error, ProtectedRoute } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
   AddJob,
@@ -13,8 +13,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
-          <Route path='stats' element={<Stats />} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
           <Route path='all-jobs' element={<AllJob />} />
           <Route path='add-job' element={<AddJob />} />
           <Route path='profile' element={<Profile />} />
--- client/src/pages/dashboard/SharedLayout.jsx
+++ client/src/pages/dashboard/SharedLayout.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import Wrapper from '../../assets/wrappers/SharedLayout';
 
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Link to='add-job'>add job</Link>
+        <Link to='all-jobs'>add jobs</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
