1699535076966a0cb0d87a713d35fd38e6134494
Phuc Le <phuclevinh2000@gmail.com>
2022-07-04 21:57:49 +0300

    job status server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,6 +2,7 @@ import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
+import mongoose from 'mongoose';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -65,7 +66,26 @@ const updateJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res.send('show stats job');
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = []
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, deleteJob, getAllJob, updateJob, showStats };
