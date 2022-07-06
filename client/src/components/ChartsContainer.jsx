84ac870b1c8fd8fda7d868367d4e4182e8398887
Phuc Le <phuclevinh2000@gmail.com>
2022-07-06 21:45:14 +0300

    chart

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import AreaChartComponent from './AreaChartComponent';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import BarChartComponent from './BarChartComponent';
 
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
