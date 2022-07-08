e5301147826a00266bd03c2aa4ed32de7c17bd04
Phuc Le <phuclevinh2000@gmail.com>
2022-07-09 01:22:00 +0300

    pagination server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -72,12 +72,19 @@ const getAllJob = async (req, res) => {
     result = result.sort('-position');
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   // chain sort conditions
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
