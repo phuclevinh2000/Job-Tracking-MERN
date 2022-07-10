ef51b03d5bf365c56c3248e6e5503ac00b0eaf44
Phuc Le <phuclevinh2000@gmail.com>
2022-07-10 13:10:46 +0300

    deploy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
 const router = express.Router();
 
+import rateLimiter from 'express-rate-limit';
+
+const apiLimitter = rateLimiter({
+  windowMs: 15 * 60 * 1000, //15 mins,
+  max: 10,
+  message:
+    'Too many requests from this IP address, please try again after 15 mins',
+});
+
 import { register, login, updateUser } from '../controllers/authController.js';
 import authenticateUser from '../middleware/auth.js';
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/register').post(apiLimitter, register);
+router.route('/login').post(apiLimitter, login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default router;
